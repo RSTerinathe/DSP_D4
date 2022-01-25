51ab20de95bca681f4c3126ff13ae66eac25ca8b
Rowan Terinathe <rterinathe@hotmail.com>
2022-01-25 15:55:06 +0100

    Updated data

--- bin/calculateDrivingTimes.py
+++ bin/calculateDrivingTimes.py
@@ -13,13 +13,14 @@ config.read("../config/config.ini")
 api_key = config['HERE_MAPS_KEY']['api_key']
 
 routefinder = HereMapsEmergencyRouteFinder(api_key)
-origin_locations = pd.read_csv('../data/parkingspots_revised.csv')
+# origin_locations = pd.read_csv('../data/parkingspots_revised.csv')
 # demand_locations = pd.read_csv('../data/buurten_information.csv')
+origin_locations = pd.read_csv('../data/firestations.csv')
 demand_locations = pd.read_csv('../data/wijken.csv')
 print(f"Calculating the travelling time for {len(origin_locations) * len(demand_locations)} combinations")
 i = 0
 
-csv_file = open('../data/travelling_times.csv', 'w', newline='')
+csv_file = open('../data/travelling_times_firestations.csv', 'w', newline='')
 csv_writer = csv.writer(csv_file, delimiter =',')
 csv_writer.writerow(['origin_coords', 'demand_coords', 'travel_time', 'wijkcode', 'wijknaam'])
 nr = 0
@@ -36,7 +37,7 @@ for demand_point in demand_locations.iterrows():
         origin_lng = origin[1]['longitude']
 
         # If the straight line distance is less than 3.5 km then we calculate the travel time
-        if calculateDistanceFromCoordinates(origin_lat, origin_lng, demand_lat, demand_lng) < 3500:
+        if calculateDistanceFromCoordinates(origin_lat, origin_lng, demand_lat, demand_lng) < 3700:
             # Calculate travelling time between two points
             travelling_time = routefinder.getEmergencyRoute(origin_lat, origin_lng, demand_lat, demand_lng)
             sleep(0.1)
